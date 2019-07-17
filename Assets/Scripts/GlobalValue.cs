71389a4c8f395fa59c7de8e2fab7d2154828e05e
ajmenghan <344249384@qq.com>
2019-07-17 18:57:43 +0800

    update

--- Assets/Scripts/GlobalValue.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GlobalValue {
-
-    public static int MAP_WIDTH = 3;
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# AnotherWorld
-2D 休闲智力游戏 Unity引擎开发
